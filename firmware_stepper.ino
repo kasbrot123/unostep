c30508b5f46113d145dcd91217f2bfca3c6307c9
Michael T <michael@computer>
2024-09-03 11:08:34 +0200

    added firmware for arduino and python unostep with class to control motors

---
+++ firmware_stepper.ino
@@ -0,0 +1,211 @@
+
+const int stepM1 = 2;
+const int dirM1  = 5;
+
+const int stepM2 = 3;
+const int dirM2  = 6;
+
+const int stepM3 = 4;
+const int dirM3  = 7;
+
+const int enPin = 8;
+
+int stepDelay = 50; // Speed (max. 7 microseconds)
+
+long positionM1 = 0;
+long positionM1goal = 0;
+long positionM2 = 0;
+long positionM2goal = 0;
+long positionM3 = 0;
+long positionM3goal = 0;
+
+const long stepsPerRevolution = 6400;
+
+void setup() {
+    pinMode(stepM1, OUTPUT);
+    pinMode(dirM1, OUTPUT);
+
+    pinMode(stepM2, OUTPUT);
+    pinMode(dirM2, OUTPUT);
+
+    pinMode(stepM3, OUTPUT);
+    pinMode(dirM3, OUTPUT);
+
+    pinMode(enPin, OUTPUT);
+
+    // digitalWrite(enPin, LOW); // Power On
+    digitalWrite(enPin, HIGH); // Power Off
+
+    Serial.begin(115200);
+}
+
+void loop() {
+    if (Serial.available()) {
+        String command = Serial.readString();
+        command.trim();
+
+        // Position
+        if (command.startsWith("P1")) {
+            //Serial.print("Position M1: ");
+            Serial.println(positionM1);
+            return;
+        } else if (command.startsWith("P2")) {
+            //Serial.print("Position M2: ");
+            Serial.println(positionM2);
+            return;
+        } else if (command.startsWith("P3")) {
+            //Serial.print("Position M3: ");
+            Serial.println(positionM3);
+            return;
+        }
+
+        // Set position to zero
+        if (command.startsWith("Z1")) {
+            Serial.println("ok");
+            // Serial.print("Position M1 set to zero.");
+            positionM1 = 0;
+            return;
+        } else if (command.startsWith("Z2")) {
+            Serial.println("ok");
+            // Serial.print("Position M2 set to zero.");
+            positionM2 = 0;
+            return;
+        } else if (command.startsWith("Z3")) {
+            Serial.println("ok");
+            // Serial.print("Position M3 to zero.");
+            positionM3 = 0;
+            return;
+        }
+
+        int motor_number = 0;
+        bool clockwise = true;
+        long steps = 0;
+
+        // Choose Motor
+        if (command.startsWith("M1")) {
+            motor_number = 1;
+        } 
+        else if (command.startsWith("M2")) {
+            motor_number = 2;
+        }
+        else if (command.startsWith("M3")) {
+            motor_number = 3;
+        } 
+        else {
+            Serial.println("Invalid motor number");
+            // Serial.print(command.substring(0,3));
+            return;
+        }
+
+        // Return to position 0 
+        if (command.endsWith("P0")) {
+            if (motor_number == 1) {
+                steps = positionM1;
+                clockwise = (steps < 0);
+                steps = abs(steps);
+            } else if (motor_number == 2) {
+                steps = positionM2;
+                clockwise = (steps < 0);
+                steps = abs(steps);
+            } else if (motor_number == 3) {
+                steps = positionM3;
+                clockwise = (steps < 0);
+                steps = abs(steps);
+            }
+        } 
+        
+        else if (command.substring(2,3) == "P") {
+                if (motor_number == 1) {
+                positionM1goal = command.substring(3).toInt();
+                steps = positionM1goal - positionM1;
+                clockwise = (steps >= 0);
+                steps = abs(steps);
+                } else if (motor_number == 2) {
+                positionM2goal = command.substring(3).toInt();
+                steps = positionM2goal - positionM2;
+                clockwise = (steps >= 0);
+                steps = abs(steps);
+              } else if (motor_number == 3) {
+                positionM3goal = command.substring(3).toInt();
+                steps = positionM3goal - positionM3;
+                clockwise = (steps >= 0);
+                steps = abs(steps);
+              }
+              }
+
+        else if (command.substring(2, 3) == "-") {
+              clockwise = false;
+              steps = command.substring(3).toInt();
+              } else {
+              clockwise = true;
+              steps = command.substring(2).toInt();
+              } 
+              
+
+            // Position update
+            if (motor_number == 1) {
+                if (clockwise) {
+                    positionM1 += steps;
+                } else {
+                    positionM1 -= steps;
+                }
+            } else if (motor_number == 2) {
+                if (clockwise) {
+                    positionM2 += steps;
+                } else {
+                    positionM2 -= steps;
+                }
+            } else if (motor_number == 3) {
+                if (clockwise) {
+                    positionM3 += steps;
+                } else {
+                    positionM3 -= steps;
+                }
+            }
+        
+
+        digitalWrite(enPin, LOW); // Power On
+
+        // Debugging
+        // Serial.print("Motor: ");
+        // Serial.print(motor_number);
+        // Serial.print(", Direction: ");
+        // Serial.print(clockwise ? "Clockwise" : "Counterclockwise");
+        // Serial.print(", Steps: ");
+        // Serial.println(steps);
+
+
+        // Control Motor
+        if (motor_number == 1) {
+            digitalWrite(dirM1, clockwise ? LOW : HIGH);
+            for (long x = 0; x < steps; x++) {
+                digitalWrite(stepM1, HIGH);
+                delayMicroseconds(stepDelay);
+                digitalWrite(stepM1, LOW);
+                delayMicroseconds(stepDelay);
+            }
+        } 
+        else if (motor_number == 2) {
+            digitalWrite(dirM2, clockwise ? LOW : HIGH);
+            for (long x = 0; x < steps; x++) {
+                digitalWrite(stepM2, HIGH);
+                delayMicroseconds(stepDelay);
+                digitalWrite(stepM2, LOW);
+                delayMicroseconds(stepDelay);
+            }
+        }
+        else if (motor_number == 3) {
+            digitalWrite(dirM3, clockwise ? LOW : HIGH);
+            for (long x = 0; x < steps; x++) {
+                digitalWrite(stepM3, HIGH);
+                delayMicroseconds(stepDelay);
+                digitalWrite(stepM3, LOW);
+                delayMicroseconds(stepDelay);
+            }
+        }
+
+        Serial.println("ok");
+
+        digitalWrite(enPin, HIGH); // Power Off
+    }
+    }
---
+++ unostep.py
@@ -0,0 +1,186 @@
+import time
+import serial
+from serial.tools import list_ports
+import os
+
+"""
+class
+
+
+"""
+
+
+def serial_is_connected(serial):
+    if serial is None:
+        return False
+
+    is_connected = True
+    # problem was:
+    # - it interferes with arduino firmware
+    # - it writes every time when sending a command
+    # - it has to wait every time 
+    # -> no good
+    # try:
+    #     serial.write('?'.encode())
+    #     serial.flush()
+    # except:
+    #     is_connected = False
+        # send ping and see if something is comming back
+        # or check serial command
+        # TO BE IMPLEMENTED
+    return is_connected
+
+
+
+class Motor:
+
+    def __init__(self, name):
+        self.name = name
+        self.connection = None
+
+    def setSerial(self, Serial):
+        self.connection = Serial
+
+
+    def isConnected(self):
+        return serial_is_connected(self.connection)
+
+
+    def command(self, Command):
+        if not self.isConnected():
+            return False
+
+        # answer = self.connection.readlines()
+        # print('first answer', answer)
+
+        # self.connection.flush()
+        self.connection.write(Command.encode())
+        # answer = self.connection.readline()
+        answer = self.connection.read_until('\n'.encode()).decode()[:-2]
+        print('second answer', answer)
+
+        if answer != 'ok':
+            return False
+
+        return True
+
+
+    def steps(self, Steps):
+
+        Command = 'M'+self.name+str(int(Steps)) + '\n'
+        return self.command(Command)
+
+    def position(self, Position):
+
+        Command = 'M' + self.name+ 'P' + str(int(Position)) + '\n'
+        return self.command(Command)
+
+
+
+
+
+
+class Hydra:
+
+    def __init__(self):
+        self.name_com_port = ''
+        self.connection = None
+        self.X = Motor('1')
+        self.Y = Motor('2')
+        self.Z = Motor('3')
+        # self.A = Motor('A')
+
+    def connect(self, name_port = None):
+
+        if name_port is None:
+            avail_com_ports = list_ports.comports()
+            n_comports = len(avail_com_ports)
+            if n_comports == 1:
+                self.name_com_port = avail_com_ports[0].device
+            elif n_comports > 1:
+                print('There is more than one COM port.')
+                for i in avail_com_ports:
+                    print(i)
+                return False
+            else:
+                print("No available COM port")
+                return False
+        else:
+            self.name_com_port = name_port
+
+
+        # irgendwie fehler abfangen 
+        self.connection = serial.Serial(
+            port='COM3',
+            baudrate=115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=10
+        )
+
+        self.X.setSerial(self.connection)
+        self.Y.setSerial(self.connection)
+        self.Z.setSerial(self.connection)
+
+        # every one needs a pause from time to time
+        time.sleep(2)
+        # i dont understand why but the arduino does not catch the first command
+        # when there is no pause
+
+        return True
+    
+    def disconnect(self):
+        self.connection.close()
+
+        return True
+
+
+
+    def isConnected(self):
+        return serial_is_connected(self.connection)
+
+
+
+STEP = 10000
+
+shield = Hydra()
+shield.connect()
+print('start turning')
+shield.Y.steps(STEP)
+shield.Z.steps(STEP)
+shield.Y.steps(-STEP)
+shield.disconnect()
+
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+
+# ser.isOpen()
+#
+#
+# input=1
+# while 1 :
+#     # get keyboard input
+#     input = raw_input(">> ")
+#         # Python 3 users
+#         # input = input(">> ")
+#     if input == 'exit':
+#         ser.close()
+#         exit()
+#     else:
+#         # send the character to the device
+#         # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+#         ser.write(input + '\r\n')
+#         out = ''
+#         # let's wait one second before reading output (let's give device time to answer)
+#         time.sleep(1)
+#         while ser.inWaiting() > 0:
+#             out += ser.read(1)
+#             
+#         if out != '':
+#             print ">>" + out
+#
+#
+
+
+
